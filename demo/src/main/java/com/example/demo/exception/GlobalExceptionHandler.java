758fbebbd6682c74404b55051c1bf4d5725d1a52
PhamDacToan <toan.phamdac2003@gmail.com>
2025-03-20 17:27:02 +0700

    just config redis

---
+++ demo/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.demo.exception;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice // manage all global exception
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value = RuntimeException.class)
+    ResponseEntity<String> handlingRuntimeException(RuntimeException exception){
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    }
